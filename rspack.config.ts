00ec51d0457e37d42e2884fda795b6b6c733aa71
nrmnqdds <quddusnuriman@gmail.com>
2024-07-06 23:58:17 +0800

    :sparkles: added tanstack query

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,27 +1,34 @@
 //@ts-ignore
 import { TanStackRouterRspack } from "@tanstack/router-plugin/rspack";
 
+import path from "node:path";
+
 module.exports = {
-  plugins: [TanStackRouterRspack()],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: {
-                  tailwindcss: {},
-                  autoprefixer: {},
-                },
-              },
-            },
-          },
-        ],
-        type: "css",
-      },
-    ],
-  },
+	plugins: [TanStackRouterRspack()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: "postcss-loader",
+						options: {
+							postcssOptions: {
+								plugins: {
+									tailwindcss: {},
+									autoprefixer: {},
+								},
+							},
+						},
+					},
+				],
+				type: "css",
+			},
+		],
+	},
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
+import { QueryProvider } from "@/components/providers/query-provider";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-
 import { routeTree } from "./routeTree.gen";
 
 // Set up a Router instance
 const router = createRouter({
-  routeTree,
-  defaultPreload: "intent",
+	routeTree,
+	defaultPreload: "intent",
 });
 
 // Register things for typesafety
 declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+	interface Register {
+		router: typeof router;
+	}
 }
 const App = () => {
-  return <RouterProvider router={router} />;
+	return (
+		<QueryProvider>
+			<RouterProvider router={router} />
+		</QueryProvider>
+	);
 };
 
 export default App;
