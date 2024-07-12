4051bce23eec798a98ec6f5cae8a170c299d24ab
nrmnqdds <quddusnuriman@gmail.com>
2024-07-12 19:26:17 +0800

    :label: added types to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 module.exports = {
+  darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
